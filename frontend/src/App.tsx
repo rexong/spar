d5d768cf1ca3e7febb69e8f6792b837cdae13291
rexong <rexonghanwei200035@gmail.com>
2025-05-31 00:24:30 +0800

    feat: add path for metrics and evaluators

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,6 +9,14 @@ import GoalsList from "./pages/goals/goals-list";
 import GoalsCreate from "./pages/goals/goals-create";
 import GoalsEdit from "./pages/goals/goals-edit";
 import GoalsView from "./pages/goals/goals-view";
+import MetricsList from "./pages/metrics/metrics-list";
+import MetricsCreate from "./pages/metrics/metrics-create";
+import MetricsEdit from "./pages/metrics/metrics-edit";
+import MetricsView from "./pages/metrics/metrics-view";
+import EvaluatorsList from "./pages/evaluators/evaluators-list";
+import EvaluatorsCreate from "./pages/evaluators/evaluators-create";
+import EvaluatorsEdit from "./pages/evaluators/evaluators-edit";
+import EvaluatorsView from "./pages/evaluators/evaluators-view";
 
 function App() {
   return (
@@ -26,6 +34,14 @@ function App() {
             <Route path="/goals/create" element={<GoalsCreate />} />
             <Route path="/goals/edit/:id" element={<GoalsEdit />} />
             <Route path="/goals/:id" element={<GoalsView />} />
+            <Route path="/metrics" element={<MetricsList/>} />
+            <Route path="/metrics/create" element={<MetricsCreate />} />
+            <Route path="/metrics/edit/:id" element={<MetricsEdit />} />
+            <Route path="/metrics/:id" element={<MetricsView />} />
+            <Route path="/evaluators" element={<EvaluatorsList />} />
+            <Route path="/evaluators/create" element={<EvaluatorsCreate />} />
+            <Route path="/evaluators/edit/:id" element={<EvaluatorsEdit />} />
+            <Route path="/evaluators/:id" element={<EvaluatorsView />} />
           </Routes>
         </div>
       </div>
